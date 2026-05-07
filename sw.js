71e0d075411b9cb559b79a59c63d115ebd4aec5e
angelmicelti <angelmicelti@users.noreply.github.com>
2026-05-07 13:40:11 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // Service Worker para PWA - Plan de Mejora IES Virgen de Villadiego
 
-const CACHE_NAME = 'plan-mejora-v1.5';
+const CACHE_NAME = 'plan-mejora-v1.6';
 
 // Recursos estáticos a cachear en la instalación
 const URLS_TO_CACHE = [
